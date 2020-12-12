ba88578b6b4526c6a4454b92d815483c32ffb0a6
wanglu <376638623@qq.com>
2020-12-13 00:49:56 +0900

    '删除一些错误router'

--- src/router/index.js
+++ src/router/index.js
@@ -149,27 +149,7 @@ export const constantRouterMap = [
       }
     ]
   },
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
+
 
   {
     path: '/form',
